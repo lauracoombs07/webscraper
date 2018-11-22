dafb9e52d5b7a043452eea78b553e80133938565
Laura <laura.coombs07@gmail.com>
2018-11-22 00:26:22 -0700

    screenshotting

--- example.js
+++ example.js
@@ -1,11 +1,11 @@
-//const puppeteer = require('puppeteer');
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
+//const puppeteer = require('puppeteer-core');
  
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'example.png'});
+  await page.goto('https://google.com');
+  await page.screenshot({path: 'screenshots/google3.png', fullPage: true});
  
   await browser.close();
 })();
