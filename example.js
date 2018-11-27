02b7977d0e5be40e2933dd2534f44547b817def4
Laura <laura.coombs07@gmail.com>
2018-11-26 20:23:35 -0700

    testing text scrape

--- example.js
+++ example.js
@@ -1,11 +1,31 @@
 const puppeteer = require('puppeteer');
 //const puppeteer = require('puppeteer-core');
- 
+//const url = process.argv[2];
+// if (!url) {
+//     throw "Please provide a URL as the first argument";
+// } 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://google.com');
-  await page.screenshot({path: 'screenshots/google3.png', fullPage: true});
- 
+  await page.goto('https://www.nbcnews.com/');
+  await page.pdf({path: 'screenshots/testfalse.pdf'});
+  await page.setRequestInterception(false);
+        page.on('request', request => {
+        request.respond({
+        status: 404,
+        contentType: 'text/plain',
+        body: 'Not Found!'
+  });
+});
   await browser.close();
 })();
+
+// async function run () {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto(url);
+//   await page.screenshot({path: 'screenshot.png'});
+//   browser.close();
+// }
+// run();
+
